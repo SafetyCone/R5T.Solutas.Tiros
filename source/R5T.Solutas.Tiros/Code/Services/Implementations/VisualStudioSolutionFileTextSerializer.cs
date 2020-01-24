daa2f6c495e3a2c6105e6563a2186e304f197b08
MinexAutomation <minex.automation@gmail.com>
2020-01-24 13:32:08 -0800

    Added VisualStudioSolutionFileTextSerializer implementation.

---
+++ source/R5T.Solutas.Tiros/Code/Services/Implementations/VisualStudioSolutionFileTextSerializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+using R5T.Cambridge.Types;
+using R5T.Magyar.IO.Extensions;
+using R5T.Tiros;
+
+
+namespace R5T.Solutas.Tiros
+{
+    public class VisualStudioSolutionFileTextSerializer : ITextSerializer<SolutionFile>
+    {
+        public SolutionFile Deserialize(TextReader reader)
+        {
+            var solutionFile = new SolutionFile();
+
+            while(reader.ReadLine(out var line))
+            {
+                solutionFile.ContentLines.Add(line);
+            }
+
+            return solutionFile;
+        }
+
+        public void Serialize(TextWriter writer, SolutionFile solutionFile)
+        {
+            foreach (var line in solutionFile.ContentLines)
+            {
+                writer.WriteLine(line);
+            }
+        }
+    }
+}
