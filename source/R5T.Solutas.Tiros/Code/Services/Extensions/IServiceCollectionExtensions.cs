66d2232c305a1a3bf770b7232c4643d7cf87c532
MinexAutomation <minex.automation@gmail.com>
2020-01-30 17:41:34 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Solutas.Tiros/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Cambridge.Types;
+using R5T.Dacia;
+using R5T.Tiros;
+
+
+namespace R5T.Solutas.Tiros
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="VisualStudioSolutionFileTextSerializer"/> implementation of <see cref="ITextSerializer{T}"/>, T: <see cref="SolutionFile"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddVisualStudioSolutionFileTextSerializer(this IServiceCollection services)
+        {
+            services.AddSingleton<ITextSerializer<SolutionFile>, VisualStudioSolutionFileTextSerializer>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="VisualStudioSolutionFileTextSerializer"/> implementation of <see cref="ITextSerializer{T}"/>, T: <see cref="SolutionFile"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<ITextSerializer<SolutionFile>> AddVisualStudioSolutionFileTextSerializerAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<ITextSerializer<SolutionFile>>(() => services.AddVisualStudioSolutionFileTextSerializer());
+            return serviceAction;
+        }
+    }
+}
