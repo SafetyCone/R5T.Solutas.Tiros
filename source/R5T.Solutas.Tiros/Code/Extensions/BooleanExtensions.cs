feecfc6d12f91bb35b6b83fe7fcdffa59ac5e108
MinexAutomation <minex.automation@gmail.com>
2020-01-31 22:58:49 -0800

    Added boolean extensions.

---
+++ source/R5T.Solutas.Tiros/Code/Extensions/BooleanExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using R5T.Magyar.Extensions;
+
+
+namespace R5T.Solutas.Tiros
+{
+    public static class BooleanExtensions
+    {
+        public static string ToStringSolutionFileFormat(this bool value)
+        {
+            var representation = value.ToStringAllCapitals();
+            return representation;
+        }
+    }
+}
